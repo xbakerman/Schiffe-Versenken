12157f73b4f07af47bef9eb8a7ce8c84809cedc9
xbakerman <nm2689@mci4me.at>
2024-05-27 23:38:50 +0200

    Kommunikation

--- include/main.h
+++ include/main.h
@@ -3,6 +3,17 @@
 
 #include <stm32f0xx.h>
 
-
+void delay(uint32_t time);
+void USART2_IRQHandler(void);
+void UserButton_Init(void);
+void send_start_message(void);
+void send_field_checksum(void);
+void process_incoming_message(void);
+void generate_field(void);
+void place_ship(int start_row, int start_col, int size, int is_vertical);
+void place_ship_randomly(int size);
+void send_shot(uint8_t col, uint8_t row);
+void handle_shot_report(char result);
+void game_loop(void);
 
 #endif // MAIN_H_
--- src/main.c
+++ src/main.c
@@ -1,49 +1,83 @@
-/*
-----------------------------------------------------------------------------
- * Notes:
- * This project is currently under construction and has not yet been fully tested.
- * Use with caution.
- * ----------------------------------------------------------------------------
- */
-
 #include <stm32f0xx.h>
 #include "epl_clock.h"
 #include "epl_usart.h"
+#include "main.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 
 #define DEBUG
+#define FIELD_SIZE 10
+#define EMPTY 0 
+#define SHIP 1
+#define HIT 2
+#define MISS 3
+#define NUMBER_OF_SHIPS 10
 
+
+
+uint8_t shipsize[NUMBER_OF_SHIPS] = {5, 4, 4, 3, 3, 3, 2, 2, 2, 2};	
 uint8_t data[200];
 uint8_t data_idx = 0;
 bool newline_rcvd = false;
+uint8_t field[FIELD_SIZE][FIELD_SIZE];
+uint8_t opponent_field[FIELD_SIZE][FIELD_SIZE];
 
-/**
- * @brief Delays the program execution for a specified amount of time.
- * @param time The amount of time to delay in number of cycles.
- * @return 0 when the delay is completed.
- */
-int delay(uint32_t time)
+uint8_t last_shot_col = 0;
+uint8_t last_shot_row = 0;
+bool my_turn = false;
+
+typedef enum {
+    STATE_INIT,
+    STATE_WAIT_START,
+    STATE_GENERATE_FIELD,
+    STATE_SEND_FIELD_CHECKSUM,
+    STATE_WAIT_OPPONENT_CHECKSUM,
+    STATE_PLAY,
+    STATE_WAIT_SHOT_REPORT,
+    STATE_SEND_SHOT_REPORT,
+    STATE_GAMEEND,
+    SEND_START_AFTER_CS
+} State;
+
+State current_state = STATE_INIT;
+
+void delay(uint32_t time)
 {
     for(uint32_t i = 0; i < time; i++ ){
-        asm("nop"); // No operation, used for delaying
+        asm("nop");
     }
     return 0;
 }
 
+void delay_ms(uint32_t ms) {
+    for (uint32_t i = 0; i < ms * 1000; i++) {
+        asm("nop");
+    }
+}
+
+void debug_log(const char* message) {
+    EPL_usart_write_n_bytes((uint8_t*)message, strlen(message));
+    //message++;
+    //delay_ms(7000);
+}
+
+void debug_log_char(uint8_t character) {
+    EPL_usart_write_n_bytes(&character, 1);
+    //delay_ms(7000);
+}
 
 void USART2_IRQHandler()
 {
     if ( USART2->ISR & USART_ISR_RXNE ) {
         uint8_t c = USART2->RDR;
-        //appendFIFO(c, epl_rx_buffer);
-
         if (data_idx >= sizeof(data)) {
             return;
         }
 
         data[data_idx] = c;
-        data_idx ++;
+        data_idx++;
         if (c == '\n') {
             newline_rcvd = true;
         }
@@ -52,120 +86,224 @@ void USART2_IRQHandler()
 
 void UserButton_Init(void) 
 {
-    // GPIOC Clock Enable
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-
-    // PC13 als Eingang konfigurieren
-    GPIOC->MODER &= ~(GPIO_MODER_MODER13); // Input mode (00)
-    GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR13); // No pull-up, no pull-down (00)
+    GPIOC->MODER &= ~(GPIO_MODER_MODER13);
+    GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR13);
 }
 
-size_t findNewline(const uint8_t *data, size_t size)
+void send_start_message()
 {
-    for (size_t i = 0; i < size; i++){
-        if (data[i] == '\n'){
-            return i+1;
+    uint8_t start_msg[] = "START11815630\n";
+    EPL_usart_write_n_bytes(start_msg, sizeof(start_msg) - 1);
+}
+
+void send_field_checksum() {
+    uint8_t checksum_msg[13] = "CS";
+    int idx = 2;
+    for (int col = 0; col < FIELD_SIZE; col++) {
+        int count = 0;
+        for (int row = 0; row < FIELD_SIZE; row++) {
+            if (field[row][col] == SHIP) {
+                count++;
+            }
+        }
+        checksum_msg[idx++] = '0' + count;
+    }
+    checksum_msg[idx++] = '\n';
+    EPL_usart_write_n_bytes(checksum_msg, idx);
+    current_state = STATE_WAIT_OPPONENT_CHECKSUM;
+}
+
+void process_incoming_message() {
+    if (newline_rcvd) {
+        newline_rcvd = false;
+        data[data_idx] = '\0'; // Null-terminate the string for debugging
+        //debug_log("Received: ");
+        //debug_log((char*)data);
+        //debug_log("\n");
+
+        if (strncmp((char*)data, "CS", 2) == 0) {
+            current_state = SEND_START_AFTER_CS;
+        ;
+        } else if (strncmp((char*)data, "BOOM", 4) == 0) {
+            uint8_t col = data[4] - '0';
+            uint8_t row = data[5] - '0';
+            char result = (field[row][col] == SHIP) ? 'T' : 'W';
+            field[row][col] = (field[row][col] == SHIP) ? HIT : MISS;
+            EPL_usart_write_n_bytes((uint8_t*)&result, 1);
+            EPL_usart_write_n_bytes((uint8_t*)"\n", 1);
+            if (result == 'W') {
+                my_turn = true;
+            }
+            current_state = STATE_PLAY;
+        } else if (data[0] == 'W' || data[0] == 'T') {
+            handle_shot_report(data[0]);
+        } else if (strncmp((char*)data, "START", 5) == 0) {
+            current_state = STATE_GENERATE_FIELD;
+        }
+        data_idx = 0;
+    }
+}
+
+void generate_field()
+{
+    for(uint8_t i = 0; i < FIELD_SIZE; i++) {
+        for(uint8_t j = 0; j < FIELD_SIZE; j++) {
+           field[i][j] = EMPTY;
+           opponent_field[i][j] = EMPTY;
         }
     }
-    return size;
+
+    for (int i = 0; i < NUMBER_OF_SHIPS; i++) {
+        place_ship_randomly(shipsize[i]);
+    }
 }
 
-typedef enum
-{
-  STATE_START,
-  STATE_GENERATE_FIELD_MESSAGE,
-  STATE_WAIT_FOR_FIELD,
-  STATE_GAME_LOOP,
+bool can_place_ship(int start_row, int start_col, int size, int is_vertical) {
+    if (is_vertical) {
+        if (start_row + size > FIELD_SIZE) return false;
+        for (int i = start_row; i < start_row + size; i++) {
+            if (field[i][start_col] != EMPTY) return false;
+        }
+    } else {
+        if (start_col + size > FIELD_SIZE) return false;
+        for (int i = start_col; i < start_col + size; i++) {
+            if (field[start_row][i] != EMPTY) return false;
+        }
+    }
+    return true;
 }
 
-State;
-State current_state = STATE_START;
-
-void handle_start()
-{
-  if (newline_rcvd)
-    {
-        if (strncmp(data, "START", 5) == 0)
-        {
-            uint8_t checksum[] = "Hello\n";
-            EPL_usart_write_n_bytes(checksum, findNewline(data, sizeof(data)));
-            newline_rcvd = false;
-            //memset(data, 0, sizeof(data));
-            data_idx = 0; 
+void place_ship(int start_row, int start_col, int size, int is_vertical) {
+    if (is_vertical) {
+        for (int i = start_row; i < start_row + size; i++) {
+            field[i][start_col] = SHIP;
+        }
+    } else {
+        for (int i = start_col; i < start_col + size; i++) {
+            field[start_row][i] = SHIP;
         }
     }
 }
 
-void generate_start_message()
-{
+void place_ship_randomly(int size) {
+    bool placed = false;
+    while (!placed) {
+        int start_row = rand() % FIELD_SIZE;
+        int start_col = rand() % FIELD_SIZE;
+        int is_vertical = rand() % 2;
 
-  uint8_t start_msg[] = "START11815630\n";
-
-  if (!(GPIOC->IDR & (1UL << 13))==1)
-    {
-        for (uint8_t i = 0; i < sizeof(start_msg); i++)
-        {
-            while (!(USART2->ISR & USART_ISR_TXE)); // Warten, bis das TXE-Flag gesetzt ist
-            USART2->TDR = start_msg[i]; // Senden Sie das nächste Byte
+        if (can_place_ship(start_row, start_col, size, is_vertical)) {
+            place_ship(start_row, start_col, size, is_vertical);
+            placed = true;
         }
+    }
+}
 
-        for (uint8_t i = 0; i < 2000000; i++)   //Button entprellen
-            asm("nop");
+void send_shot(uint8_t col, uint8_t row) {
+    uint8_t shot_msg[7];
+    shot_msg[0] = 'B';
+    shot_msg[1] = 'O';
+    shot_msg[2] = 'O';
+    shot_msg[3] = 'M';
+    shot_msg[4] = '0' + col;  // Spalte in ASCII-Zeichen konvertieren
+    shot_msg[5] = '0' + row;  // Zeile in ASCII-Zeichen konvertieren
+    shot_msg[6] = '\n';
+    
+    EPL_usart_write_n_bytes(shot_msg, 7);
+    current_state = STATE_WAIT_SHOT_REPORT;
+}
+
+void handle_shot_report(char result) {
+    if (result == 'T') {
+        opponent_field[last_shot_row][last_shot_col] = HIT;
+        my_turn = true; 
+        current_state = STATE_PLAY; // Continue shooting if it's a hit
+    } else {
+        opponent_field[last_shot_row][last_shot_col] = MISS;
+        my_turn = false;  
     }
 
-    current_state = STATE_GENERATE_FIELD_MESSAGE;
+    // Set state to play to check for the next move
+    current_state = STATE_PLAY;
+}
+
+void game_loop() {
+    switch (current_state) {
+        case STATE_INIT:
+            //debug_log("State: INIT\n");
+            if ((GPIOC->IDR & GPIO_IDR_13) == 0) {
+                my_turn = true;
+                send_start_message();
+                current_state = STATE_GENERATE_FIELD;
+            } else {
+                current_state = STATE_WAIT_START;
+            }
+            break;
+        case SEND_START_AFTER_CS:
+            //debug_log("State: SEND_START_AFTER_CS\n");
+            send_start_message();
+            current_state = STATE_PLAY;
+            break;
         
-        
-        //EPL_usart_write_n_bytes(start_msg, findNewline(data, sizeof(data)));
-        //newline_rcvd = false;
-        //memset(data, 0, sizeof(data));
-        //data_idx = 0;
+        case STATE_WAIT_START:
+            //debug_log("State: WAIT_START\n");
+            process_incoming_message();
+            break;
+        case STATE_GENERATE_FIELD:
+            //debug_log("State: GENERATE_FIELD\n");
+            generate_field();
+            send_field_checksum();
+            break;
+        case STATE_WAIT_OPPONENT_CHECKSUM:
+            //debug_log("State: WAIT_OPPONENT_CHECKSUM\n");
+            process_incoming_message();
+            break;
+        case STATE_PLAY:
+            //debug_log("State: PLAY\n");
+            if (my_turn) {
+                uint8_t col = rand() % FIELD_SIZE;
+                uint8_t row = rand() % FIELD_SIZE;
+                send_shot(col, row);
+                last_shot_col = col;
+                last_shot_row = row;
+                my_turn = false;
+            
+            } else {
+                process_incoming_message();
+            }
+            break;
+        case STATE_WAIT_SHOT_REPORT:
+            //debug_log("State: WAIT_SHOT_REPORT\n");
+            process_incoming_message();
+            break;
+        case STATE_SEND_SHOT_REPORT:
+            //debug_log("State: SEND_SHOT_REPORT\n");
+            // Implement shot reporting logic
+            break;
+        case STATE_GAMEEND:
+            //debug_log("State: GAMEEND\n");
+            // Handle game end logic
+            break;
+        default:
+            //debug_log("State: UNKNOWN\n");
+            break;
+    }
 }
 
-
-int main(void){
-
-
-
+int main(void) {
     epl_usart_t myusart;
     myusart.baudrate = 9600;
     myusart.fifo_size = 20;
 
-    // Configure the system clock to 48MHz
     EPL_SystemClock_Config();
     EPL_init_usart(&myusart);
+    UserButton_Init();
+    current_state = STATE_INIT;
 
-    //generate_start_message();
-    
-
-    while (1){
-
-        //if (newline_rcvd)
-        //{
-        //uint8_t msg[] = "Hello, World \r\n";
-        //EPL_usart_write_n_bytes(msg, findNewline(data, sizeof(data)));
-        //newline_rcvd = false;
-        ////memset(data, 0, sizeof(data));
-        //data_idx = 0;
-        //}
-
-        switch (current_state)
-        {
-        case STATE_START:
-            UserButton_Init();
-            generate_start_message();
-            break;
-        case STATE_GENERATE_FIELD_MESSAGE:
-            handle_start();
-            break;
-        
-        default:
-            break;
-        }
-        
+    while (1) {
+        game_loop();
     }
 
-    
-  
-    
+    return 0;
 }
